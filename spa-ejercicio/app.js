2b3439448a89593c2a4f2cde16f137b7e78f50cf
jpgcano <juan-0395@hotmail.com>
2026-01-15 08:07:40 -0500

    Mis avances locales

--- spa-ejercicio/app.js
+++ spa-ejercicio/app.js
@@ -1,19 +1,70 @@
 const app = document.querySelector('#app');
 
-function renderHome(){
-    app.innerHTML = '<h1><p>Bienvenido a nuestra SPA</p></h1>';
+function renderHome() {
+    app.innerHTML = '<h1><p>🏠​ Bienvenido a nuestra SPA</p></h1>';
 }
 
-function renderServices(){
-    app.innerHTML='<h1>Servicios</h1> <p>Frontend con JS</p>';
+function renderServices() {
+    app.innerHTML = '<h1>Servicios</h1> <p>Frontend con JS</p>';
 }
 
-function renderContact(){
-    app.innerHTML ='<h1>Contacto</h1> <p>Clan@halmilton.dev</p>';
+function renderContact() {
+    app.innerHTML = '<h1>Contacto</h1> <p>Clan@halmilton.dev</p>';
+}
+function renderAbout() {
+    app.innerHTML = '<h1>Error 404</h1> <p>Pagina en construcción</p>';
+
+}
+function renderNotFound() {
+    app.innerHTML = '<h1>Error 404</h1> <p>Pagina no encontrada</p>';
 }
 
-document.querySelector("#home").addEventListener('click',renderHome);
-document.querySelector("#services").addEventListener('click',renderServices);
-document.querySelector("#contac").addEventListener('click',renderContact);
+let counter = 0;
+
+function renderCounter() {
+    app.innerHTML = `
+    <h1> Contador</h1>
+    <p>${counter}</p>
+    <button id='add'>+</button>
+    `;
+    document.querySelector('#add').onclick = () => {
+        counter++;
+        renderCounter();
+    };
+}
+
+
+function router() {
+    const route = location.hash;
+
+    switch (route) {
+        case '#/home':
+            renderHome();
+            break;
+        case '#/services':
+            renderServices();
+            break;
+        case '#/contac':
+            renderContact();
+            break;
+        case '#/about':
+            renderAbout();
+            break;
+        case '#/counter':
+            renderCounter();
+            break;
+        default:
+            renderHome()
+    }
+}
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
+
+document.querySelector("#home").addEventListener('click', renderHome);
+document.querySelector("#services").addEventListener('click', renderServices);
+document.querySelector("#contac").addEventListener('click', renderContact);
 
 renderHome();
+
+
